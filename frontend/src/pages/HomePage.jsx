16357a5e6005c476f80e5c838e8e42240ecd1f4e
michaelkagiri <michaelkagiri6@gmail.com>
2026-02-08 03:39:34 +0300

    ecom

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -68,8 +68,8 @@ function HomePage() {
           </div>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
+            {products.map((products) => (
+              <ProductCard key={products.id} product={products} />
             ))}
           </div>
         )}
